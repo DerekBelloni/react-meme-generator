60e3a725abd00f61bd51314e99a2c3ab89ce674a
Derek <derekbelloni10@gmail.com>
2022-05-20 11:21:46 -0600

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header--title">Meme Generator</h1>
+      <h4 className="header-project">React Course - Project 3</h4>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style.css';
+
+const Meme = () => {
+  return (
+    <div className="meme--content">
+      <input className="meme--input--left" type="text" name="" id="" />
+      <input className="meme--input--right" type="text" name="" id="" />
+    </div>
+  )
+}
+
+export default Meme;
