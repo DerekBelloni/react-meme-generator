42c3a47884396823ba3eea87490bfc3be3ab3755
Derek <derekbelloni10@gmail.com>
2022-05-22 10:34:42 -0600

    useState method written in Meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style.css';
 
+import memesData from "../memesData";
+
 const Meme = () => {
+  const [memeImage, setMemeImage] = useState('')
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    console.log(memesArray[randomNumber])
+    setMemeImage(memesArray[randomNumber].url)
+  }
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input className="form--input" type="text" placeholder="top text" name="" id="" />
         <input className="form--input" type="text" placeholder="bottom text" name="" id="" />
-        <button className="form--btn">Get a new meme image 🖼</button>
-      </form>
+        <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼</button>
+      </div>
+      <img src={memeImage} alt="" />
     </main>
   )
 }
