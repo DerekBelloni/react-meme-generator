d48d29cbc38ff4bc5db30c682e03bf1d4e6f5b84
Derek <derekbelloni10@gmail.com>
2022-05-23 12:04:51 -0600

    revisions to state in meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,12 +5,23 @@ import memesData from "../memesData";
 
 const Meme = () => {
   const [memeImage, setMemeImage] = useState('')
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
+
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes
+    const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    console.log(memesArray[randomNumber])
-    setMemeImage(memesArray[randomNumber].url)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
   }
   return (
     <main>
