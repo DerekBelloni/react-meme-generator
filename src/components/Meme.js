41d2c4fb7a65e78cd9af2cce4831fd2036158425
Derek <derekbelloni10@gmail.com>
2022-06-01 19:33:23 -0600

    meme component rendering text from input fields

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -23,15 +23,28 @@ const Meme = () => {
       randomImage: url
     }))
   }
+
+  function handleChange(event) {
+    const { name, value } = event.target
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }))
+    console.log("[top text]:", meme.topText)
+    console.log("[bottom text]:", meme.bottomText)
+  }
+
   return (
     <main>
       <div className="form">
-        <input className="form--input" type="text" placeholder="top text" name="" id="" />
-        <input className="form--input" type="text" placeholder="bottom text" name="" id="" />
+        <input className="form--input" type="text" placeholder="top text" name="topText" value={meme.topText} onChange={handleChange} />
+        <input className="form--input" type="text" placeholder="bottom text" name="bottomText" value={meme.bottomText} onChange={handleChange} />
         <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
-      <div className="meme-img">
-        <img src={meme.randomImage} alt="" />
+      <div className="meme">
+        <img className="meme--image" src={meme.randomImage} alt="" />
+        <h2 className="meme--text--top">{meme.topText}</h2>
+        <h2 className="meme--text--bottom">{meme.bottomText}</h2>
       </div>
     </main>
   )
